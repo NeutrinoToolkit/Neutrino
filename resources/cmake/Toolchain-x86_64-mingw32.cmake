9d631b31898c2a3eb37875320ce320f49467b67f
Alessandro Flacco <alex@mellonta.net>
2015-12-17 17:08:40 +0100

    * x86_64 toolchain (not validated)

--- resources/cmake/Toolchain-x86_64-mingw32.cmake
+++ resources/cmake/Toolchain-x86_64-mingw32.cmake
@@ -14,7 +14,7 @@ SET(CMAKE_C_COMPILER /usr/bin/x86_64-w64-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER /usr/bin/x86_64-w64-mingw32-c++)
 SET(CMAKE_RC_COMPILER /usr/bin/x86_64-w64-mingw32-windres)
 
-SET(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
+SET(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
@@ -24,3 +24,18 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 # dep tree to be found in cross-compile-win, one level up neutrino tree
+include_directories("${CMAKE_CURRENT_LIST_DIR}/../../../cross-compile-win/include")
+SET(CMAKE_FIND_ROOT_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../cross-compile-win)
+
+# look for qt5
+# qt subtree to be located in ${CMAKE_FIND_ROOT_PATH}
+#set(Qt5Widgets_DIR ${CMAKE_FIND_ROOT_PATH}/qt/lib/cmake/Qt5Widgets)
+#find_package(Qt5Widgets)
+#set(Qt5Core_DIR ${CMAKE_FIND_ROOT_PATH}/qt/lib/cmake/Qt5Core)
+#find_package(Qt5Core)
+
+set(Qt5_DIR ${CMAKE_FIND_ROOT_PATH}/qt5/lib/cmake/Qt5)
+set(QWT_LIBRARY ${CMAKE_FIND_ROOT_PATH}/Qwt-6.1.2/lib/libqwt.a)
+set(QWT_INCLUDE_DIR ${CMAKE_FIND_ROOT_PATH}/Qwt-6.1.2/include)
+
+
