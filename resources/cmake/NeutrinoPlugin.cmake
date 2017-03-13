a07932dc37391c9135a61a66ff1741098aaf32c7
Tommaso Vinci <tommaso.vinci@polytechnique.edu>
2017-03-13 16:31:33 +0100

    remove ashes

--- resources/cmake/NeutrinoPlugin.cmake
+++ resources/cmake/NeutrinoPlugin.cmake
@@ -4,7 +4,25 @@ MACRO(ADD_NEUTRINO_PLUGIN)
     PROJECT (${MY_PROJECT_NAME} CXX)
 
     MESSAGE(STATUS "Plugin ${PROJECT_NAME}")
-    include(FindNeutrinoGuiComponents)
+
+    include(FindNeutrinoDeps)
+
+    if(DEFINED ENV{QTDIR})
+        set(CMAKE_PREFIX_PATH $ENV{QTDIR} ${CMAKE_PREFIX_PATH})
+    endif()
+
+    if(DEFINED QTDIR)
+        set(CMAKE_PREFIX_PATH ${QTDIR} ${CMAKE_PREFIX_PATH})
+    endif()
+
+    if (NOT DEFINED Qt5_DIR AND APPLE)
+        set(Qt5_DIR "/usr/local/opt/qt5/lib/cmake/Qt5")
+    endif()
+
+    find_package(Qt5 COMPONENTS Core Gui Sql Widgets Svg PrintSupport UiTools Multimedia MultimediaWidgets OpenGL REQUIRED)
+
+    add_definitions(${QT_DEFINITIONS})
+    include_directories(${CMAKE_BINARY_DIR} ${CMAKE_CURRENT_BINARY_DIR})
 
 
     if(APPLE)
