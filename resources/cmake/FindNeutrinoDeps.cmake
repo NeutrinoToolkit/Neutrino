1c628ca7cf995b21bbf30a19fec8672087b77812
Tommaso Vinci <tommaso.vinci@polytechnique.edu>
2018-01-28 21:41:18 +0100

    fix fedora build

--- resources/cmake/FindNeutrinoDeps.cmake
+++ resources/cmake/FindNeutrinoDeps.cmake
@@ -1,145 +1,145 @@
 
 find_package(OpenMP)
 if (OPENMP_FOUND AND NOT "${CMAKE_CXX_FLAGS}" MATCHES "^(${OpenMP_CXX_FLAGS})")
-    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-    set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${OpenMP_CXX_FLAGS}")
-    add_definitions(-DHAVE_OPENMP)
+	set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+	set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${OpenMP_CXX_FLAGS}")
+	add_definitions(-DHAVE_OPENMP)
 endif()
 
 find_package( ZLIB REQUIRED )
 if ( ZLIB_FOUND )
-    include_directories( ${ZLIB_INCLUDE_DIRS} )
-    set(LIBS ${LIBS} ${ZLIB_LIBRARIES} )
-    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        message (STATUS "using ZLIB: ${ZLIB_LIBRARIES}")
-    endif()
+	include_directories( ${ZLIB_INCLUDE_DIRS} )
+	set(LIBS ${LIBS} ${ZLIB_LIBRARIES} )
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		message (STATUS "using ZLIB: ${ZLIB_LIBRARIES}")
+	endif()
 endif( ZLIB_FOUND )
 
 
 find_package(TIFF REQUIRED)
 if (TIFF_FOUND)
-    include_directories(${TIFF_INCLUDE_DIRS})
-    set(LIBS ${LIBS} ${TIFF_LIBRARIES})
-    add_definitions(-DHAVE_LIBTIFF)
-    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        message (STATUS "using TIFF: ${TIFF_LIBRARIES}")
-    endif()
+	include_directories(${TIFF_INCLUDE_DIRS})
+	set(LIBS ${LIBS} ${TIFF_LIBRARIES})
+	add_definitions(-DHAVE_LIBTIFF)
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		message (STATUS "using TIFF: ${TIFF_LIBRARIES}")
+	endif()
 endif()
 
 #fftw
 find_library(FFTW NAMES fftw3 fftw3-3 REQUIRED)
 if (NOT ${FFTW} STREQUAL "FFTW-NOTFOUND")
-    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        message (STATUS "using FFTW: ${FFTW}")
-    endif()
-    set(LIBS ${LIBS} ${FFTW})
-    add_definitions(-DHAVE_LIBFFTW)
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		message (STATUS "using FFTW: ${FFTW}")
+	endif()
+	set(LIBS ${LIBS} ${FFTW})
+	add_definitions(-DHAVE_LIBFFTW)
 endif()
 
 #in precompiled win dlls the threads are included
 if(NOT WIN32)
-    # fftw_threads
-    find_library(FFTW_THREADS NAMES fftw3_threads REQUIRED)
-    if (NOT ${FFTW_THREADS} STREQUAL "FFTW_THREADS-NOTFOUND")
-        if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-            message (STATUS "using FFTW_THREADS: ${FFTW_THREADS}")
-        endif()
-        set(LIBS ${LIBS} ${FFTW_THREADS})
-    endif()
+	# fftw_threads
+	find_library(FFTW_THREADS NAMES fftw3_threads REQUIRED)
+	if (NOT ${FFTW_THREADS} STREQUAL "FFTW_THREADS-NOTFOUND")
+		if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+			message (STATUS "using FFTW_THREADS: ${FFTW_THREADS}")
+		endif()
+		set(LIBS ${LIBS} ${FFTW_THREADS})
+	endif()
 endif()
 
 #gsl
 find_library(GSL NAMES gsl)
 if (NOT ${GSL} STREQUAL "GSL-NOTFOUND")
-    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        message (STATUS "using gsl: ${GSL}")
-    endif()
-    set(LIBS ${LIBS} ${GSL})
-    add_definitions(-DHAVE_LIBGSL)
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		message (STATUS "using gsl: ${GSL}")
+	endif()
+	set(LIBS ${LIBS} ${GSL})
+	add_definitions(-DHAVE_LIBGSL)
 
-    FIND_PATH(GSL_INCLUDE_DIR gsl/gsl_math.h
-        /usr/local/include/
-        /usr/include
-        )
-    IF (GSL_INCLUDE_DIR)
-    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        message (STATUS "gsl header dir: ${GSL_INCLUDE_DIR}")
-    endif()
-    include_directories(BEFORE "${GSL_INCLUDE_DIR}")
-    ENDIF (GSL_INCLUDE_DIR)
+	FIND_PATH(GSL_INCLUDE_DIR gsl/gsl_math.h
+		/usr/local/include/
+		/usr/include
+		)
+	IF (GSL_INCLUDE_DIR)
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		message (STATUS "gsl header dir: ${GSL_INCLUDE_DIR}")
+	endif()
+	include_directories(BEFORE "${GSL_INCLUDE_DIR}")
+	ENDIF (GSL_INCLUDE_DIR)
 endif()
 
 # gslcblas
 find_library(GSLCBLAS NAMES gslcblas)
 if (NOT ${GSLCBLAS} STREQUAL "GSLCBLAS-NOTFOUND")
-    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        message (STATUS "using gslcblas: ${GSL}")
-    endif()
-    set(LIBS ${LIBS} ${GSLCBLAS})
-    add_definitions(-DHAVE_LIBGSLCBLAS)
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		message (STATUS "using gslcblas: ${GSL}")
+	endif()
+	set(LIBS ${LIBS} ${GSLCBLAS})
+	add_definitions(-DHAVE_LIBGSLCBLAS)
 else()
-    message(FATAL_ERROR "Missing gslcblas.")
+	message(FATAL_ERROR "Missing gslcblas.")
 endif()
 
 # hdf4
 find_library (HDF4 NAMES mfhdf)
 if (NOT ${HDF4} STREQUAL "HDF4-NOTFOUND")
-    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        message (STATUS "using hdf4: ${HDF4}")
-    endif()
-    set(LIBS ${LIBS} ${HDF4})
-    add_definitions(-DHAVE_LIBMFHDF)
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		message (STATUS "using hdf4: ${HDF4}")
+	endif()
+	set(LIBS ${LIBS} ${HDF4})
+	add_definitions(-DHAVE_LIBMFHDF)
 
-    FIND_PATH(HDF4_INCLUDE_DIR hdf.h
-        /usr/local/include/
-        /usr/include
-        /usr/local/include/hdf
-        /usr/include/hdf
-        ${CMAKE_FIND_ROOT_PATH}/include
-        )
+	FIND_PATH(HDF4_INCLUDE_DIR hdf.h
+		/usr/local/include/
+		/usr/include
+		/usr/local/include/hdf
+		/usr/include/hdf
+		${CMAKE_FIND_ROOT_PATH}/include
+		)
 
-    IF (HDF4_INCLUDE_DIR)
-    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        message (STATUS "hdf4 header dir: ${HDF4_INCLUDE_DIR}")
-    endif()
-    include_directories(BEFORE "${HDF4_INCLUDE_DIR}")
-    ENDIF (HDF4_INCLUDE_DIR)
+	IF (HDF4_INCLUDE_DIR)
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		message (STATUS "hdf4 header dir: ${HDF4_INCLUDE_DIR}")
+	endif()
+	include_directories(BEFORE "${HDF4_INCLUDE_DIR}")
+	ENDIF (HDF4_INCLUDE_DIR)
 endif()
 
 find_library (DF NAMES df)
 if (NOT ${DF} STREQUAL "DF-NOTFOUND")
-    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        message (STATUS "using df: ${DF}")
-    endif()
-    set(LIBS ${LIBS} ${DF})
-    add_definitions(-DHAVE_LIBDF)
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		message (STATUS "using df: ${DF}")
+	endif()
+	set(LIBS ${LIBS} ${DF})
+	add_definitions(-DHAVE_LIBDF)
 endif()
 
 find_package(JPEG REQUIRED)
 if (JPEG_FOUND)
-    include_directories(${JPEG_INCLUDE_DIRS})
-    set(LIBS ${LIBS} ${JPEG_LIBRARIES})
-    add_definitions(-DHAVE_JPEG)
+	include_directories(${JPEG_INCLUDE_DIRS})
+	set(LIBS ${LIBS} ${JPEG_LIBRARIES})
+	add_definitions(-DHAVE_JPEG)
 endif()
 
 find_library(CFITS NAMES cfitsio)
 if (NOT ${CFITS} STREQUAL "CFITS-NOTFOUND")
-    FIND_PATH(CFITS_INCLUDE_DIR fitsio.h
-        /usr/include
-        /usr/include/netpbm
-        /usr/local/include
-        /usr/local/include/netpbm
-        ${CMAKE_FIND_ROOT_PATH}/include
-        ${CMAKE_FIND_ROOT_PATH}/include/cfitsio
-        )
-    IF (CFITS_INCLUDE_DIR)
-	    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-			message(STATUS "using cfits: ${CFITS}")
-		endif()
-		list(APPEND LIBS ${CFITS})
-		add_definitions(-DHAVE_LIBCFITSIO)
-		include_directories(BEFORE ${CFITS_INCLUDE_DIR})
-	endif()
+	FIND_PATH(CFITS_INCLUDE_DIR fitsio.h
+		/usr/include
+		/usr/include/cfitsio
+		/usr/local/include
+		/usr/local/include/cfitsio
+		${CMAKE_FIND_ROOT_PATH}/include
+		${CMAKE_FIND_ROOT_PATH}/include/cfitsio
+		)
+	IF (CFITS_INCLUDE_DIR)
+	IF (CMAKE_BUILD_TYPE STREQUAL "Debug")
+	    message(STATUS "using cfits: ${CFITS}")
+	ENDIF()
+	list(APPEND LIBS ${CFITS})
+	add_definitions(-DHAVE_LIBCFITSIO)
+	include_directories(BEFORE ${CFITS_INCLUDE_DIR})
+	ENDIF()
 endif()
 
 
@@ -148,20 +148,20 @@ endif()
 find_package(OpenCL QUIET)
 
 if (OpenCL_FOUND)
-    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        message (STATUS "using OpenCL: ${OpenCL_LIBRARIES}")
-    endif()
+	if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+		message (STATUS "using OpenCL: ${OpenCL_LIBRARIES}")
+	endif()
 
-    include_directories(${OpenCL_INCLUDE_DIRS})
-    set(LIBS ${LIBS} ${OpenCL_LIBRARIES})
-    add_definitions(-DHAVE_OPENCL)
+	include_directories(${OpenCL_INCLUDE_DIRS})
+	set(LIBS ${LIBS} ${OpenCL_LIBRARIES})
+	add_definitions(-DHAVE_OPENCL)
 
-    find_library (CLFFT NAMES clFFT)
-    if (NOT ${CLFFT} STREQUAL "CLFFT-NOTFOUND")
-        if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-            message (STATUS "using clFFT: ${CLFFT}")
-        endif()
-        set(LIBS ${LIBS} ${CLFFT})
-        add_definitions(-DHAVE_LIBCLFFT)
-    endif()
+	find_library (CLFFT NAMES clFFT)
+	if (NOT ${CLFFT} STREQUAL "CLFFT-NOTFOUND")
+		if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+			message (STATUS "using clFFT: ${CLFFT}")
+		endif()
+		set(LIBS ${LIBS} ${CLFFT})
+		add_definitions(-DHAVE_LIBCLFFT)
+	endif()
 endif()
