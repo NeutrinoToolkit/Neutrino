de26a4e0860c02c70a0e8be366c87e44d970087c
Tommaso Vinci <tommaso.vinci@polytechnique.edu>
2018-03-16 21:10:51 +0100

    revert

--- plugins/Thomson_Parabola/tpGlDraw.cc
+++ plugins/Thomson_Parabola/tpGlDraw.cc
@@ -4,7 +4,7 @@
 tpGlDraw::tpGlDraw(QWidget *parent)
     : QGLWidget(parent)
 {
-//    setFormat(QGLFormat(QGL::DoubleBuffer | QGL::DepthBuffer));
+    setFormat(QGLFormat(QGL::DoubleBuffer | QGL::DepthBuffer));
 
     // report resize
     resize(parent->size());
@@ -23,12 +23,10 @@ tpGlDraw::tpGlDraw(QWidget *parent)
     faceColors[1] = Qt::green;
     faceColors[2] = Qt::blue;
     faceColors[3] = Qt::yellow;
-    qDebug() << "created";
 }
 
 void tpGlDraw::initializeGL()
 {
-    qDebug() << "HERE";
     qglClearColor(Qt::white);
     glShadeModel(GL_FLAT);
     glEnable(GL_DEPTH_TEST);
@@ -37,7 +35,6 @@ void tpGlDraw::initializeGL()
 
 void tpGlDraw::resizeGL(int width, int height)
 {
-    qDebug() << "HERE";
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -49,33 +46,31 @@ void tpGlDraw::resizeGL(int width, int height)
 
 void tpGlDraw::paintGL()
 {
-    qDebug() << "HERE";
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     draw();
 }
 
 void tpGlDraw::draw()
 {
-    qDebug() << "HERE";
 
-	// e tutta la spiegazione sta qui:
-	// gluLookAt is equivalent to
-            
-	//glMultMatrixf(M);
-	//glTranslated(-eyex, -eyey, -eyez);
-	//
-	// from: https://www.opengl.org/sdk/docs/man2/xhtml/gluLookAt.xml
+    // e tutta la spiegazione sta qui:
+    // gluLookAt is equivalent to
 
-	static GLfloat P1[3] = { 1.0, -1.0, 2. };
-	static GLfloat P2[3] = { 0, -1.0, 2. };
+    //glMultMatrixf(M);
+    //glTranslated(-eyex, -eyey, -eyez);
+    //
+    // from: https://www.opengl.org/sdk/docs/man2/xhtml/gluLookAt.xml
 
-    	static GLfloat P3[3] = { 0, -1.0, 3. };
-    	static GLfloat P4[3] = { 1.0, -1., 3. };
+    static GLfloat P1[3] = { 1.0, -1.0, 2. };
+    static GLfloat P2[3] = { 0, -1.0, 2. };
 
-	GLfloat M[16] = {    magnification,0,0,0,
-				    0,magnification,0,0,
-				    0,0,magnification,0,
-				    0,0,0,magnification};
+        static GLfloat P3[3] = { 0, -1.0, 3. };
+        static GLfloat P4[3] = { 1.0, -1., 3. };
+
+    GLfloat M[16] = {    magnification,0,0,0,
+                    0,magnification,0,0,
+                    0,0,magnification,0,
+                    0,0,0,magnification};
 //
 //    static const GLfloat * const coords[4][3] = {
 //        { P1, P2, P3 }, { P1, P3, P4 }, { P1, P4, P2 }, { P2, P4, P3 }
@@ -108,35 +103,35 @@ void tpGlDraw::draw()
 //		    std::cerr<<std::endl;
 //    }
 //    std::cerr<<"============================================================================"<<std::endl;
-	
+
     
     // draw axis
-	glLoadName(1);
-	glBegin(GL_LINES);
-	qglColor(Qt::black);
+    glLoadName(1);
+    glBegin(GL_LINES);
+    qglColor(Qt::black);
 
 
-	// get min/max
-	fp minv, maxv;
-	for (int ii=0; ii<boxes.size(); ii++) {
-		minv = min(minv, boxes[ii]->myvertex1);
-		minv = min(minv, boxes[ii]->myvertex2);
-		
-		maxv = max(maxv, boxes[ii]->myvertex1);
-		maxv = max(maxv, boxes[ii]->myvertex2);
-	}
+    // get min/max
+    fp minv, maxv;
+    for (int ii=0; ii<boxes.size(); ii++) {
+        minv = min(minv, boxes[ii]->myvertex1);
+        minv = min(minv, boxes[ii]->myvertex2);
 
-	glVertex3f(1.5*minv.x(),0,0);
-	glVertex3f(1.5*maxv.x(),0,0);
-	glVertex3f(0,1.5*minv.y(),0);
-	glVertex3f(0,1.5*maxv.y(),0);
-	glVertex3f(0,0,1.5*minv.z());
-	glVertex3f(0,0,1.5*maxv.z());
-	glEnd();
+        maxv = max(maxv, boxes[ii]->myvertex1);
+        maxv = max(maxv, boxes[ii]->myvertex2);
+    }
 
-	renderText(1.6*maxv.x(),0,0,"x");
-	renderText(0,1.6*maxv.y(),0,"y");
-	renderText(0,0,1.6*maxv.z(),"z");
+    glVertex3f(1.5*minv.x(),0,0);
+    glVertex3f(1.5*maxv.x(),0,0);
+    glVertex3f(0,1.5*minv.y(),0);
+    glVertex3f(0,1.5*maxv.y(),0);
+    glVertex3f(0,0,1.5*minv.z());
+    glVertex3f(0,0,1.5*maxv.z());
+    glEnd();
+
+    renderText(1.6*maxv.x(),0,0,"x");
+    renderText(0,1.6*maxv.y(),0,"y");
+    renderText(0,0,1.6*maxv.z(),"z");
 
 /*
 //
@@ -149,49 +144,46 @@ void tpGlDraw::draw()
 //            glVertex3f(coords[i][j][0], coords[i][j][1],
 //                       coords[i][j][2]);
 //        }
-	glVertex3f(P1[0], P1[1], P1[2]);
-	glVertex3f(P2[0], P2[1], P2[2]);
-	glVertex3f(P3[0], P3[1], P3[2]);
-	glVertex3f(P4[0], P4[1], P4[2]);
+    glVertex3f(P1[0], P1[1], P1[2]);
+    glVertex3f(P2[0], P2[1], P2[2]);
+    glVertex3f(P3[0], P3[1], P3[2]);
+    glVertex3f(P4[0], P4[1], P4[2]);
         glEnd();
 //    }
 */
 
-	//GLfloat cp1[3] = {0,0,0};
-	//GLfloat cp2[3] = {2,2,2};
-	//generateCube(cp1, cp2, 2);
+    //GLfloat cp1[3] = {0,0,0};
+    //GLfloat cp2[3] = {2,2,2};
+    //generateCube(cp1, cp2, 2);
 
-	for (int ii=0; ii<boxes.size(); ii++) {
-		//std::cerr<<"---- Adding tribox "<<boxes[ii]->myvertex1<<" <--> "<<boxes[ii]->myvertex2<<std::endl;
-		generateTribox(*boxes[ii], Qt::blue);
-	}
-	//generateTribox(tribox(fp(-2,-2,3), fp(2,2,5)), Qt::blue);
-	//generateTribox(tribox(fp(-4,-1,6), fp(4,1,9)), Qt::yellow);
+    for (int ii=0; ii<boxes.size(); ii++) {
+        //std::cerr<<"---- Adding tribox "<<boxes[ii]->myvertex1<<" <--> "<<boxes[ii]->myvertex2<<std::endl;
+        generateTribox(*boxes[ii], Qt::blue);
+    }
+    //generateTribox(tribox(fp(-2,-2,3), fp(2,2,5)), Qt::blue);
+    //generateTribox(tribox(fp(-4,-1,6), fp(4,1,9)), Qt::yellow);
 
 }
 
 void tpGlDraw::wheelEvent(QWheelEvent *ev) {
-    qDebug() << ev;
-	int nDeg = ev->delta()/8;
-	int nSteps = nDeg/15;
+    int nDeg = ev->delta()/8;
+    int nSteps = nDeg/15;
 
-	ev->accept();
+    ev->accept();
 
-	magnification += nSteps/10.;
-	//qDebug()<<"magnification: "<<magnification;
-	updateGL();
+    magnification += nSteps/10.;
+    //qDebug()<<"magnification: "<<magnification;
+    updateGL();
 }
 
 void tpGlDraw::mousePressEvent(QMouseEvent *event)
 {
-    qDebug() << "HERE";
     lastPos = event->pos();
     //qDebug()<<"mouse event: "<<event->buttons();
 }
 
 void tpGlDraw::mouseMoveEvent(QMouseEvent *event)
 {
-    qDebug() << "HERE";
     GLfloat dx = GLfloat(event->x() - lastPos.x()) / width();
     GLfloat dy = GLfloat(event->y() - lastPos.y()) / height();
     if (event->buttons() & Qt::LeftButton) {
@@ -203,9 +195,9 @@ void tpGlDraw::mouseMoveEvent(QMouseEvent *event)
         rotationZ -= 180 * dx;
         updateGL();
     } else if (event->buttons() & Qt::MidButton) {
-	translationX += dx;
-	translationY += dy;
-	updateGL();
+    translationX += dx;
+    translationY += dy;
+    updateGL();
     }
     lastPos = event->pos();
 
@@ -213,7 +205,6 @@ void tpGlDraw::mouseMoveEvent(QMouseEvent *event)
 
 void tpGlDraw::mouseDoubleClickEvent(QMouseEvent *event)
 {
-    qDebug() << "HERE";
     int face = faceAtPosition(event->pos());
     if (face != -1) {
         QColor color = QColorDialog::getColor(faceColors[face], this);
@@ -226,7 +217,6 @@ void tpGlDraw::mouseDoubleClickEvent(QMouseEvent *event)
 
 int tpGlDraw::faceAtPosition(const QPoint &pos)
 {
-    qDebug() << "HERE";
     const int MaxSize = 512;
     GLuint buffer[MaxSize];
     GLint viewport[4];
@@ -261,56 +251,54 @@ int tpGlDraw::faceAtPosition(const QPoint &pos)
 
 void tpGlDraw::generateCube(GLfloat *P1, GLfloat *P2, int num)
 {
-    qDebug() << "HERE";
     glLoadName(num);
-	glBegin(GL_QUAD_STRIP);
-	//qglColor(Qt::red);
+    glBegin(GL_QUAD_STRIP);
+    //qglColor(Qt::red);
 
-	glVertex3f(P1[0], P1[1], P1[2]);
-	glVertex3f(P1[0], P1[1], P2[2]);
+    glVertex3f(P1[0], P1[1], P1[2]);
+    glVertex3f(P1[0], P1[1], P2[2]);
 
-	glVertex3f(P1[0], P2[1], P1[2]);
-	glVertex3f(P1[0], P2[1], P2[2]);
+    glVertex3f(P1[0], P2[1], P1[2]);
+    glVertex3f(P1[0], P2[1], P2[2]);
 
-	glVertex3f(P2[0], P2[1], P1[2]);
-	glVertex3f(P2[0], P2[1], P2[2]);
+    glVertex3f(P2[0], P2[1], P1[2]);
+    glVertex3f(P2[0], P2[1], P2[2]);
 
-	glVertex3f(P2[0], P1[1], P1[2]);
-	glVertex3f(P2[0], P1[1], P2[2]);
+    glVertex3f(P2[0], P1[1], P1[2]);
+    glVertex3f(P2[0], P1[1], P2[2]);
 
-	glVertex3f(P1[0], P1[1], P1[2]);
-	glVertex3f(P1[0], P1[1], P2[2]);
-	glEnd();
+    glVertex3f(P1[0], P1[1], P1[2]);
+    glVertex3f(P1[0], P1[1], P2[2]);
+    glEnd();
 
-	glLoadName(num);
-	glBegin(GL_QUADS);
-	//qglColor(Qt::red);
-	
-	glVertex3f(P1[0], P1[1], P1[2]);
-	glVertex3f(P1[0], P2[1], P1[2]);
-	glVertex3f(P2[0], P2[1], P1[2]);
-	glVertex3f(P2[0], P1[1], P1[2]);
-	
-	// questo deve girare al contrario
-	glVertex3f(P1[0], P1[1], P2[2]);
-	glVertex3f(P2[0], P1[1], P2[2]);
-	glVertex3f(P2[0], P2[1], P2[2]);
-	glVertex3f(P1[0], P2[1], P2[2]);
-	
-	glEnd();
-	
+    glLoadName(num);
+    glBegin(GL_QUADS);
+    //qglColor(Qt::red);
+
+    glVertex3f(P1[0], P1[1], P1[2]);
+    glVertex3f(P1[0], P2[1], P1[2]);
+    glVertex3f(P2[0], P2[1], P1[2]);
+    glVertex3f(P2[0], P1[1], P1[2]);
+
+    // questo deve girare al contrario
+    glVertex3f(P1[0], P1[1], P2[2]);
+    glVertex3f(P2[0], P1[1], P2[2]);
+    glVertex3f(P2[0], P2[1], P2[2]);
+    glVertex3f(P1[0], P2[1], P2[2]);
+
+    glEnd();
+
 }
 
 // draw tribox
 void tpGlDraw::generateTribox(tribox my_box, QColor color)
 {
-    qDebug() << "HERE";
     qglColor(color);
-	GLfloat cp1[3] = {(GLfloat) my_box.myvertex1.x(),
-		(GLfloat) my_box.myvertex1.y(),
-		(GLfloat) my_box.myvertex1.z()};
-	GLfloat cp2[3] = {(GLfloat) my_box.myvertex2.x(),
-		(GLfloat) my_box.myvertex2.y(),
-		(GLfloat) my_box.myvertex2.z()};
-	generateCube(cp1, cp2, 0);
+    GLfloat cp1[3] = {(GLfloat) my_box.myvertex1.x(),
+        (GLfloat) my_box.myvertex1.y(),
+        (GLfloat) my_box.myvertex1.z()};
+    GLfloat cp2[3] = {(GLfloat) my_box.myvertex2.x(),
+        (GLfloat) my_box.myvertex2.y(),
+        (GLfloat) my_box.myvertex2.z()};
+    generateCube(cp1, cp2, 0);
 }
--- plugins/Thomson_Parabola/tribox.h
+++ plugins/Thomson_Parabola/tribox.h
@@ -20,88 +20,88 @@ typedef tridimvec<double> fp;
 class tribox {
 
 public:
-	tribox()
-		: myvertex1(), myvertex2(), scalarValue(0)
-	{ }
-	tribox(fp vert1, fp vert2)
-		: myvertex1(vert1), myvertex2(vert2) 
-	{ }
+    tribox()
+        : myvertex1(), myvertex2(), scalarValue(0)
+    { }
+    tribox(fp vert1, fp vert2)
+        : myvertex1(vert1), myvertex2(vert2)
+    { }
 
-	tribox(fp vert1, fp vert2, double value)
-		: myvertex1(vert1), myvertex2(vert2), scalarValue(value)
-	{ }
+    tribox(fp vert1, fp vert2, double value)
+        : myvertex1(vert1), myvertex2(vert2), scalarValue(value)
+    { }
 
-	~tribox()
-	{ }
+    ~tribox()
+    { }
 
-	inline bool isInside(fp);
-	bool isValid(void)
-	{ return (myvertex1 != myvertex2); }
+    inline bool isInside(fp);
+    bool isValid(void)
+    { return (myvertex1 != myvertex2); }
 
-	void setV1(fp p1)
-	{ myvertex1 = p1; }
+    void setV1(fp p1)
+    { myvertex1 = p1; }
 
-	void setV2(fp p2)
-	{ myvertex2 = p2; }
+    void setV2(fp p2)
+    { myvertex2 = p2; }
 
-	void setBoundaries(fp p1, fp p2)
-	{ myvertex1 = p1; myvertex2 = p2; }
+    void setBoundaries(fp p1, fp p2)
+    { myvertex1 = p1; myvertex2 = p2; }
 
-	fp getCenter()
-	{ fp cc = 0.5*(myvertex1+myvertex2); return cc;}
+    fp getCenter()
+    { fp cc = 0.5*(myvertex1+myvertex2); return cc;}
 
-	fp getSize()
-	{ fp si = (myvertex1-myvertex2).abs(); return si;}
+    fp getSize()
+    { fp si = (myvertex1-myvertex2).abs(); return si;}
 
-	void setCenter(fp cp)
-	{
-      		fp op1 = myvertex1, op2 = myvertex2;
-		fp occ = 0.5*(op1+op2); 
-		fp dcc = cp-occ;
-      		myvertex1 = op1+dcc; 
-		myvertex2 = op2+dcc; 
+    void setCenter(fp cp)
+    {
+            fp op1 = myvertex1, op2 = myvertex2;
+        fp occ = 0.5*(op1+op2);
+        fp dcc = cp-occ;
+            myvertex1 = op1+dcc;
+        myvertex2 = op2+dcc;
 
-		fp sz = getSize();
-	}
+        fp sz = getSize();
+    }
 
-	void setSize(fp si)
-	{ fp occ = 0.5*(myvertex1+myvertex2);
+    void setSize(fp si)
+    { fp occ = 0.5*(myvertex1+myvertex2);
           myvertex1 = occ-0.5*si; myvertex2 = occ+0.5*si; }
 
-	void assignScalarQuantity(double value)
-	{ scalarValue = value; }
+    void assignScalarQuantity(double value)
+    { scalarValue = value; }
 
-	//void assignScalarQuantity(struct scalarMap *)
-	//{ }
+    //void assignScalarQuantity(struct scalarMap *)
+    //{ }
 
-	double getField(fp look_point)
-	{ if (isInside(look_point)) return scalarValue; else return 0; }
+    double getField(fp look_point)
+    { if (isInside(look_point)) return scalarValue; else return 0; }
 
 
-	fp myvertex1, myvertex2;
-	double scalarValue;
+    fp myvertex1, myvertex2;
+    double scalarValue;
 
 };
 
 inline bool
 tribox::isInside(fp cp)
 {
-	if (!isValid() )
-		return false;
+    if (!isValid() )
+        return false;
 
-	if ( (((cp.x() > myvertex1.x()) && (cp.x() < myvertex2.x())) || ((cp.x() < myvertex1.x()) && (cp.x() > myvertex2.x()))) && 
-	(((cp.y() > myvertex1.y()) && (cp.y() < myvertex2.y())) || ((cp.y() < myvertex1.y()) && (cp.y() > myvertex2.y()))) &&
-			(((cp.z() > myvertex1.z()) && (cp.z() < myvertex2.z())) || ((cp.z() < myvertex1.z()) && (cp.z() > myvertex2.z()))) ) 
-	{ return true; }
+    if ( (((cp.x() > myvertex1.x()) && (cp.x() < myvertex2.x())) || ((cp.x() < myvertex1.x()) && (cp.x() > myvertex2.x()))) &&
+    (((cp.y() > myvertex1.y()) && (cp.y() < myvertex2.y())) || ((cp.y() < myvertex1.y()) && (cp.y() > myvertex2.y()))) &&
+            (((cp.z() > myvertex1.z()) && (cp.z() < myvertex2.z())) || ((cp.z() < myvertex1.z()) && (cp.z() > myvertex2.z()))) )
+    { return true; }
 
-	return false;
+    return false;
 }
 
 inline std::ostream &
 operator<< (std::ostream &lhs, tribox &mybox)
 {
-	lhs<<"<"<<mybox.myvertex1<<"|"<<mybox.myvertex2<<">";
-	return lhs;
+    lhs<<"<"<<mybox.myvertex1<<"|"<<mybox.myvertex2<<">";
+    return lhs;
 }
 
 #endif
