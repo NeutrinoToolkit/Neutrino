7c9bd171524f606b75d1153d88e7fda850e00607
iltommi <tommaso.vinci@polytechnique.edu>
2022-11-09 13:27:34 +0100

    Update NeutrinoPlugin.cmake

--- resources/cmake/NeutrinoPlugin.cmake
+++ resources/cmake/NeutrinoPlugin.cmake
@@ -97,8 +97,8 @@ MACRO(ADD_NEUTRINO_PLUGIN)
     QT_WRAP_UI(nUIs ${NEUTRINO_ROOT}/src/neutrino.ui ${NEUTRINO_ROOT}/src/graphics/nLine.ui ${NEUTRINO_ROOT}/src/graphics/nObject.ui ${NEUTRINO_ROOT}/src/graphics/nPoint.ui)
 
     add_library (${PROJECT_NAME} SHARED ${HEADERS} ${SOURCES} ${UIS} ${nUIs} ${QRCS} ${PANDOC_QRC} ${README_MD})
-#    add_dependencies(${PROJECT_NAME} Neutrino)
-#    add_dependencies(${PROJECT_NAME} nPhysImageF)
+    add_dependencies(${PROJECT_NAME} Neutrino)
+    add_dependencies(${PROJECT_NAME} nPhysImageF)
 
     IF(APPLE)
         set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -undefined dynamic_lookup")
